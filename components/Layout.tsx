872e9786716e82eb543465746cb251fc203894ba
Thabo Kabelo Lefao <88215472+MacTech-Vators@users.noreply.github.com>
2026-03-02 18:08:41 +0200

    cleared

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import styles from "../styles/layout.module.scss";
-import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,22 +9,11 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <><div className={styles.layout}>
+    <div className={styles.layout}>
       <Navbar />
       <main className={styles.body}>{children}</main>
       <Footer />
-    </div><Html lang="en">
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1" />
-          {/* other meta tags/styles */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html></>
+    </div>
   );
 };
 
